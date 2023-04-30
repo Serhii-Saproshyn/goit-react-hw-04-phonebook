4066f2e4be91176d200c716eb2062006eab29ad5
Serhii Saproshyn <112710917+Serhii-Saproshyn@users.noreply.github.com>
2023-05-01 01:14:32 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import ContactsList from './contacts.json';
+import { nanoid } from 'nanoid';
+import { Form } from './Form/Form';
+import { Contacts } from './Contacts/Contacts';
+import { Filter } from './Filter/Filter';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) || ContactsList
+  );
+
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const onSubmiHandler = (name, number) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    const includeName = contacts.find(user => user.name === contact.name);
+    if (includeName) {
+      alert(`${contact.name} is already in contacts`);
+    } else {
+      setContacts([contact, ...contacts]);
+    }
+  };
+
+  const handelChange = e => {
+    setFilter(e.target.value);
+  };
+
+  const handleDelete = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
+
+  // render() {
+  //   const { contacts, filter } = this.state;
+  const filterContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <Form onSubmit={onSubmiHandler} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handelChange} />
+      <Contacts contacts={filterContacts} onDelete={handleDelete} />
     </div>
   );
 };
